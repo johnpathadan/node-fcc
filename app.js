c5815441f99dc01b7faf0bf6139ed00429ff1a5a
John Pathadan <john.paul@vonnue.com>
2024-01-03 18:45:03 +0530

    fix: modules intro

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const amount = 12;
+const names = require("./names");
+const { john, peter } = require("./names");
+const sayHi = require("./utils");
+const data = require("./alternitive-flavour");
 
-if (amount < 5) {
-  console.log("Small Number");
-} else {
-  console.log("Large Number");
-}
-
-console.log(`hey, it's my first node app`);
+require("./mind-grenade"); //this code will work, even
+//if we do not assign it into a variable. NOTE: when you
+//import a module, you actually invoke it
+// node app - The sum is 15
