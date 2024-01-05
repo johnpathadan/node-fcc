6721249848f8ee57001c97457c04fa905dead659
John Pathadan <john.paul@vonnue.com>
2024-01-05 14:57:27 +0530

    fix: fs module(async)

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-const { readFileSync, writeFileSync } = require("fs");
+const { readFile, writeFile } = require("fs");
 
-const first = readFileSync("./content/first.txt", "utf8");
-const second = readFileSync("./content/second.txt", "utf8");
-
-console.log(first, second);
-
-writeFileSync(
-  "./content/result-sync123.txt",
-  `Here is the result: ${first}, ${second}`,
-  { flag: "a" }
-);
+readFile("./content/first.txt", "utf8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const first = result;
+  readFile("./content/second.txt", "utf8", (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const second = result;
+    writeFile(
+      "./content/result-async.txt",
+      `Here is the result: ${first}, ${second}`,
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log(result); //undefined(it's fine)
+      }
+    );
+  });
+});
